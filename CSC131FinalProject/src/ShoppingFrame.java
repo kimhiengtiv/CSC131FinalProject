c1b3c8895e640d734db4300b7c61ce693d5ead99
kimhiengtiv <kimhiengtiv@10.0.0.6>
2020-05-04 02:52:18 -0700

    color update

--- CSC131FinalProject/src/ShoppingFrame.java
+++ CSC131FinalProject/src/ShoppingFrame.java
@@ -68,7 +68,7 @@ public class ShoppingFrame extends JFrame {
     private void addItem(final Item product, JPanel p) {
         JPanel sub = new JPanel(new FlowLayout(FlowLayout.LEFT));
         // Change center color 
-        sub.setBackground(Color.white);
+        sub.setBackground(GOLD);
         final JTextField quantity = new JTextField(3);
         quantity.setHorizontalAlignment(SwingConstants.CENTER);
         quantity.addActionListener(new ActionListener() {
